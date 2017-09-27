f2a30ed4e48a0666c68e7d863762ebe1c8001cdf
Alessandro-Commodari <commodari.a@gmail.com>
2017-09-26 23:14:42 -0400

    All parts of post component made. Only minimal styling left to adjust.

--- app/components/Post/FullPost.js
+++ app/components/Post/FullPost.js
@@ -1,10 +1,10 @@
 import PropTypes from 'prop-types';
 import React from 'react';
-import { View, Text, Button, Image } from 'react-native';
+import { View, Text, Button, Image, ScrollView } from 'react-native';
 
 import styles from './styles';
 
-const FullPost = ({ title, userImage, userName }) => {
+const FullPost = ({ title, userImage, userName, content, date, tagString }) => {
   if (!userImage) {
     userImage = require('./images/Placeholder.png');
   }
@@ -18,20 +18,29 @@ const FullPost = ({ title, userImage, userName }) => {
             resizeMode="cover"
             style={styles.icon}
             source={userImage}
-            borderRadius={40}
+            borderRadius={50}
           />
           <Text style={styles.user}>{userName}</Text>
         </View>
       </View>
-      <Text />
-      <Text />
-      <Button
-        color="green"
-        title="Request"
-        onPress={() => {
-          alert('click!');
-        }}
-      />
+      <View style={styles.body}>
+        <Text>{content}</Text>
+      </View>
+      <View style={styles.footer}>
+        <Text style={styles.date}>{date}</Text>
+        <Button
+          color="green"
+          title="Request"
+          onPress={() => {
+            alert('click!');
+          }}
+        />
+      </View>
+      <View style={styles.tagSection}>
+        <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
+          <Text>{tagString}</Text>
+        </ScrollView>
+      </View>
     </View>
   );
 };
@@ -40,6 +49,9 @@ FullPost.propTypes = {
   title: PropTypes.string,
   userImage: PropTypes.element,
   userName: PropTypes.string,
+  content: PropTypes.string,
+  date: PropTypes.string,
+  tagString: PropTypes.string,
 };
 
 export default FullPost;
--- app/components/Post/styles.js
+++ app/components/Post/styles.js
@@ -12,30 +12,57 @@ const styles = EStyleSheet.create({
     width: 0.9 * SCREEN_WIDTH,
     marginVertical: 20,
     paddingBottom: 5,
-  },
-  title: {
-    fontWeight: '800',
-    fontSize: 30,
+    elevation: 4,
   },
   header: {
     backgroundColor: '#A8DBA8',
     width: 0.9 * SCREEN_WIDTH,
     paddingHorizontal: 10,
     paddingBottom: 10,
+    paddingTop: 5,
+  },
+  title: {
+    fontWeight: '800',
+    fontSize: 35,
   },
   author: {
     flexDirection: 'row',
     alignItems: 'center',
+    paddingLeft: 10,
+    paddingTop: 5,
   },
   icon: {
-    width: 40,
-    height: 40,
+    width: 50,
+    height: 50,
   },
   user: {
     paddingHorizontal: 10,
     fontWeight: '600',
     fontSize: 20,
   },
+  body: {
+    padding: 20,
+    justifyContent: 'flex-start',
+    alignItems: 'flex-start',
+  },
+  content: {},
+  footer: {
+    width: 0.9 * SCREEN_WIDTH,
+    paddingHorizontal: 10,
+    paddingBottom: 5,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'baseline',
+  },
+  date: {
+    fontWeight: '300',
+  },
+  tagSection: {
+    width: 0.9 * SCREEN_WIDTH,
+    paddingHorizontal: 10,
+    paddingVertical: 5,
+  },
+  tags: {},
 });
 
 export default styles;
