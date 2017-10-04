90a5218ac539229a71c93fa7fc5ca7c4e16c4abc
Kevin <kevin.nam@mail.mcgill.ca>
2017-10-04 14:28:42 -0400

    Bumped expo and react-native version. Implemented Profile with Bio.

---
+++ app/components/ProfileBody/index.js
@@ -0,0 +1,4 @@
+import ProfileBody from './ProfileBody';
+import styles from './styles';
+
+export { ProfileBody, styles };
---
+++ app/components/ProfileHeader/index.js
@@ -0,0 +1,4 @@
+import ProfileHeader from './ProfileHeader';
+import styles from './styles';
+
+export { ProfileHeader, styles };
