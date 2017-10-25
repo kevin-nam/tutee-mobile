295526a18c4a6b5eed5e003e6f6731e06004a606
VittorioG <vittorio.mcgill@gmail.com>
2017-10-25 14:28:44 -0400

    Refresh now works on Connections page when you scroll down.

--- app/components/RequestCard/RequestCard.js
+++ app/components/RequestCard/RequestCard.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import {View, Text, Image, TouchableOpacity} from 'react-native';
+import { View, Text, Image, TouchableOpacity } from 'react-native';
 import styles from './styles';
 import store from '../../store/store';
 
 class RequestCard extends React.Component {
-
   constructor(props) {
     super(props);
 
@@ -19,46 +18,45 @@ class RequestCard extends React.Component {
 
     const connection = {
       uid1: store.getState().user.uid,
-      uid2: this.props.uid
+      uid2: this.props.uid,
     };
 
     const headers = new Headers({
-      "Content-Type": "application/json",
+      'Content-Type': 'application/json',
     });
 
     fetch('http://138.197.159.56:3232/connection/approve', {
       method: 'POST',
       body: JSON.stringify(connection),
-      headers: headers
+      headers: headers,
     }).then((response) => {
-      this.setState({hidden: true});
+      this.setState({ hidden: true });
       if (response.ok) {
         console.log('Successfully approved connection');
       } else {
         console.log('Failed to approve connection', connection);
       }
     });
-
   };
 
   onPressReject = () => {
     console.log('reject');
-    this.setState({hidden: true});
+    this.setState({ hidden: true });
 
     const connection = {
       uid1: store.getState().user.uid,
-      uid2: this.props.uid
+      uid2: this.props.uid,
     };
 
     const headers = new Headers({
-      "Content-Type": "application/json",
+      'Content-Type': 'application/json',
     });
 
     fetch('http://138.197.159.56:3232/connection/delete', {
       method: 'POST',
       body: JSON.stringify(connection),
-      headers: headers
-    }).then(function (response) {
+      headers: headers,
+    }).then(function(response) {
       if (response.ok) {
         console.log('Successfully rejected connection');
       } else {
@@ -71,13 +69,19 @@ class RequestCard extends React.Component {
     return (
       <View style={this.state.hidden ? styles.hidden : styles.flexVertical}>
         <View style={styles.profileImageView}>
-          <Image style={styles.profileImage} source={require('../MessagingHeader/default-user.jpg')}/>
+          <Image
+            style={styles.profileImage}
+            source={require('../MessagingHeader/default-user.jpg')}
+          />
         </View>
         <View style={styles.profileTextView}>
           <Text style={styles.profileText}>{this.props.uid}</Text>
         </View>
         <View style={styles.acceptRejectBtnView}>
-          <TouchableOpacity onPress={this.onPressAccept} style={styles.acceptBtn}>
+          <TouchableOpacity
+            onPress={this.onPressAccept}
+            style={styles.acceptBtn}
+          >
             <Text style={styles.acceptText}>Accept</Text>
           </TouchableOpacity>
           <TouchableOpacity onPress={this.onPressReject}>
