46bdfe5e627d562047a6f790ba90b2b6e594895d
Alessandro-Commodari <commodari.a@gmail.com>
2017-11-02 17:43:51 -0400

    bugfix

--- app/screens/Session.js
+++ app/screens/Session.js
@@ -1,9 +1,6 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 import { StatusBar, KeyboardAvoidingView, Text } from 'react-native';
-import { connect } from 'react-redux';
-
-import { connectAlert } from '../components/Alert';
 import { Container } from '../components/Container';
 
 class Session extends React.Component {
